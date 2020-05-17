1755dd070602746d872bc75b5938e851dbd5fdcc
Abraham <surya.saputra030090@gmail.com>
2020-05-18 00:33:24 +0700

    Tampilan, Fitur, Nonaktif & Verifikasi Akun

--- application/modules/products/views/add/photography.php
+++ application/modules/products/views/add/photography.php
@@ -1,124 +1,204 @@
-<div class="container-fluid">
+<main>
+    <div class="container z-depth-1-half tabmenu">
+        <h4 class="text-center">Tambah Item</h4>
+        <div class="row">
+            <div class="col-md-12">
+          
+              <!-- Stepers Wrapper -->
+              <ul class="stepper stepper-horizontal nav nav-tabs ">
+          
+                <!-- First Step -->
+                <li class="nav-item stepmenu-item">
+                  <a class="" id="home-tab" data-toggle="tab" href="#waiting" role="tab" aria-controls="home" aria-selected="true">
+                    <span class="circle"><i class="fas fa-dolly"></i></span>
+                    <span class="label">Item</span>
+                  </a>
+                </li>
+          
+                <!-- Second Step -->
+                <li class="nav-item stepmenu-item">
+                  <a class="" id="home-tab" data-toggle="tab" href="#onprocess" role="tab" aria-controls="home" aria-selected="true">
+                    <span class="circle"><i class="fas fa-tags"></i></span>
+                    <span class="label">Detail & Harga</span>
+                  </a>
+                </li>
+          
+                <!-- Third Step -->
+                <li class="nav-item stepmenu-item">
+                  <a id="home-tab" data-toggle="tab" href="#history" role="tab" aria-controls="home" aria-selected="true">
+                    <span class="circle"><i class="far fa-file-image"></i></span>
+                    <span class="label">Foto</span>
+                  </a>
+                </li>
+          
+              </ul>
+              <!-- /.Stepers Wrapper -->
+          
+            </div>
+          </div>
+          <div class="tab-content" id="myTabContent">
 
-    <!-- Page Heading -->
-    <div class="row">
-        <h1 class="h3 mb-4 text-gray-800 ml-3">Tambah Item</h1>
-        <a class="btn btn-primary h3 mb-4 ml-auto mr-3" href="<?= base_url('products'); ?>">Daftar Item</a>
-    </div>
-    <!-- DataTales Example -->
-    <div class="row">
-        <div class="col-sm-10 offset-1">
-            <form class="user row" method="POST" action="<?= base_url('products/tambah_produk'); ?>" enctype="multipart/form-data">
-            
-            <div class="col-6">
-                    <div class="form-group">
-                        <input type="text" name="nama_produk" class="form-control form-control-user" placeholder="Nama Produk">
-                        <input type="text" name="id_kat" value="photography" hidden>
-                        <?= form_error('nama_produk', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="form-group">
-                        <input type="text" name="harga" class="form-control form-control-user" placeholder="Biaya perhari">
-                        <?= form_error('harga', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
+            <div class="tab-pane fade" id="waiting" role="tabpanel" aria-labelledby="home-tab">
+              <form method="POST" action="<?= base_url('products/tambah_produk'); ?>" enctype="multipart/form-data">
+                <div class="card" style="box-shadow: none;">
+                    <!--Card content-->
+                    <div class="card-body pt-0">
+                        <!-- Form -->
+                            <!-- Name -->
+                            <div class="md-form m-4 mt-5">
+                                <input type="text" name="id_kat" value="photography" hidden>
+                                <input type="text" id="materialContactFormName" class="form-control" name="nama_produk">
+                                <label for="materialContactFormName">Nama Item</label>
+                            </div>
+                            <?= form_error('nama_produk', '<small class="text-danger">', '<small>'); ?>
                 
-                <div class="col-3">
-                    <div class="form-group mt-2">
-                        <input type="text" name="merk" class="form-control form-control-user" placeholder="Merk Item">
-                        <?= form_error('merk', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="form-group mt-2">
-                        <input type="text" name="stock" class="form-control form-control-user" placeholder="Stock Item">
-                        <?= form_error('stock', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="form-group">
-                        <span class="h6 mr-2">Kategori Item</span>
-                            <select name="kategori_produk" class="form-control form-control-user">
-                            <?php 
-                            foreach($kategori as $p):
-                                echo "<option value='".$p['id_kategori']."'>".$p['sub_kat']."</option>";
-                            endforeach; 
-                            ?>        
-                            </select>
-                        <?= form_error('kategori_produk', '<small class="text-danger">', '<small>'); ?>
+                            <!--Message-->
+                            <div class="md-form m-4 mt-5">
+                                <textarea id="materialContactFormMessage" class="form-control md-textarea" rows="3" name="deskripsi"></textarea>
+                                <label for="materialContactFormMessage">Deskripsi</label>
+                            </div>
+                            <?= form_error('deskripsi', '<small class="text-danger">', '<small>'); ?>
+
+                            <div class="row">
+                              <div class="col">
+                                <div class="md-form m-4 mt-5">
+                                  <input type="text" id="materialContactFormName" class="form-control" name="merk">
+                                  <label for="materialContactFormName">Merk</label>
+                                </div>
+                                <?= form_error('merk', '<small class="text-danger">', '<small>'); ?>
+                              </div>
+                              <div class="col">
+                                <div class="md-form m-4 mt-5">
+                                  <input type="number" id="materialContactFormName" class="form-control" name="stock">
+                                  <label for="materialContactFormName">Stok Item</label>
+                                </div>
+                                <?= form_error('stock', '<small class="text-danger">', '<small>'); ?>
+                              </div>
+                            </div>
+
+                            <div class="md-form mt-3 ml-5">
+                              <div class="custom-control custom-switch ml-3 mt-5">
+                                <input type="hidden" name="antar" value="0">
+                                <input type="checkbox" class="custom-control-input" id="Antar" name="antar" onclick="this.previousSibling.value=1-this.previousSibling.value">
+                                <label class="custom-control-label" for="Antar">Bersedia Mengantar</label>
+                              </div>
+                            </div>
+                
+                            <!-- Send button -->
+                            <a class="btn btn-outline-info btn-rounded z-depth-0 my-4 waves-effect" data-toggle="tab" href="#onprocess">Lanjut</a>
+                
+                        
+                        <!-- Form -->     
                     </div>
                 </div>
 
-                <div class="col-3 mt-2">
-                    <div class="form-group">
-                        <input type="text" name="deposit" class="form-control form-control-user" placeholder="Deposit">
-                        <?= form_error('deposit', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="form-group">
-                        <span class="h6 mr-2">Kondisi Item</span>
-                            <select name="kondisi" class="form-control form-control-user">
-                                <option value='1'>Baru</option>
-                                <option value='2'>Sangat Bagus</option>
-                                <option value='3'>Bagus</option>
-                                <option value='4'>Layak</option>
-                                <option value='5'>Rapuh</option>
-                            </select>
-                        <?= form_error('kondisi', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="form-group">
-                        <span class="h6 mr-2">Antar Item</span>
-                            <select name="antar" class="form-control form-control-user">
-                                <option value='2'>Tidak</option>
-                                <option value='1'>Ya</option>
-                            </select>
-                        <?= form_error('antar', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="form-group">
-                            <span class="h6">Gambar Item</span>
-                            <input type="file" name="upload_image" id="upload_image" >
-                            <?= form_error('upload_image', '<small class="text-danger">', '<small>'); ?>
+            </div>
+            <div class="tab-pane fade" id="onprocess" role="tabpanel" aria-labelledby="profile-tab">
+                <div class="card" style="box-shadow: none;">
+                    <h5 class="card-header info-color white-text text-center py-4">
+                        <strong>Detail Item</strong>
+                    </h5>
+                    <div class="card-body pt-0">
+                        <div class="row">
+                          <div class="col">
+                            <div class="md-form m-4 mt-5">
+                              <select class="custom-select custom-select-sm" id="subkat" name="kategori_produk">
+                                  <option selected>Pilih Kategori</option>
+                                  <?php 
+                                  foreach($kategori as $p):
+                                      echo "<option value='".$p['id_kategori']."'>".$p['sub_kat']."</option>";
+                                  endforeach; 
+                                  ?> 
+                                </select>
+                            </div>
+                            <?= form_error('kategori_produk', '<small class="text-danger">', '<small>'); ?>
+                          </div>
+                          <div class="col">
+                            <div class="md-form m-4 mt-5">
+                              <select class="custom-select custom-select-sm" id="subkat" name="kondisi">
+                                  <option selected>Kondisi Items</option>
+                                  <option value='1'>Baru</option>
+                                  <option value='2'>Sangat Bagus</option>
+                                  <option value='3'>Bagus</option>
+                                  <option value='4'>Layak</option>
+                                  <option value='5'>Rapuh</option>
+                                </select>
+                            </div>
+                            <?= form_error('kondisi', '<small class="text-danger">', '<small>'); ?>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col">
+                            <div class="md-form m-4 mt-5">
+                              <input type="text" id="materialContactFormName" class="form-control" name="berat">
+                              <label for="materialContactFormName">Berat</label>
+                            </div>
+                            <?= form_error('berat', '<small class="text-danger">', '<small>'); ?>
+                          </div>
+                          <div class="col">
+                            <div class="md-form m-4 mt-5">
+                              <input type="text" id="materialContactFormName" class="form-control" name="material">
+                              <label for="materialContactFormName">Material</label>
+                            </div>
+                            <?= form_error('material', '<small class="text-danger">', '<small>'); ?>
+                          </div>
+                          <div class="col">
+                            <div class="md-form m-4 mt-5">
+                                <input type="text" id="materialContactFormName" class="form-control" name="ukuran">
+                                <label for="materialContactFormName">Ukuran</label>
+                            </div>
+                            <?= form_error('ukuran', '<small class="text-danger">', '<small>'); ?>
+                          </div>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="card" style="box-shadow: none;">
+                    <h5 class="card-header info-color white-text text-center py-4">
+                        <strong>Harga Item</strong>
+                    </h5>
+                    <div class="card-body pt-0">
+                      <div class="row">
+                        <div class="col">
+                          <div class="md-form m-4 mt-5">
+                            <input type="text" id="materialContactFormName" class="form-control" name="harga">
+                            <label for="materialContactFormName">Biaya perhari</label>
+                          </div>
+                          <?= form_error('harga', '<small class="text-danger">', '<small>'); ?>
+                        </div>
+                        <div class="col">
+                          <div class="md-form m-4 mt-5">
+                            <input type="text" id="materialContactFormName" class="form-control" name="deposit">
+                            <label for="materialContactFormName">Deposito</label>
+                          </div>
+                          <?= form_error('deposit', '<small class="text-danger">', '<small>'); ?>
+                        </div>
+                      </div>
+
+                            
+                            <!-- Send button -->
+                            <a class="btn btn-outline-info btn-rounded z-depth-0 my-4 waves-effect" data-toggle="tab" href="#waiting">Kembali</a>
+                            <a class="btn btn-outline-info btn-rounded z-depth-0 my-4 waves-effect" data-toggle="tab" href="#history">Lanjut</a>
                     </div>
                 </div>
 
-                <!---- INPUT INDIVIDU -->
-                <div class="col-4">
-                    <div class="form-group">
-                        <input type="text" name="berat" class="form-control form-control-user" placeholder="Berat Item">
-                        <?= form_error('berat', '<small class="text-danger">', '<small>'); ?>
+            </div>
+            <div class="tab-pane fade" id="history" role="tabpanel" aria-labelledby="contact-tab">
+                <div class="card" style="box-shadow: none;">
+                    <div class="card-body pt-0">
+                      <img id="blah" src="#" alt="your image" style="width: 300px; max-height: 500px;"/>
+                        <div class="md-form m-4 mt-5">
+                          <div class="input-group">
+                            <div class="custom-file">
+                            <input type="file" class="custom-file-input" name="upload_image" id="upload_image" aria-describedby="inputGroupFileAddon01">
+                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                            </div>
+                          </div>
+                        </div>   
+                        <button class="btn btn-outline-info btn-rounded z-depth-0 my-4 waves-effect" type="submit" name="btnSubmit">Simpan</button>
                     </div>
                 </div>
-                <div class="col-4">
-                    <div class="form-group">
-                        <input type="text" name="material" class="form-control form-control-user" placeholder="Material Item">
-                        <?= form_error('material', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="form-group">
-                        <input type="text" name="ukuran" class="form-control form-control-user" placeholder="Ukuran Item">
-                        <?= form_error('ukuran', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                </div>
-                <!---- END INPUT INDIVIDU -->
-
-                <div class="col-12">
-                    <div class="form-group">
-                        <input type="text" name="deskripsi" class="form-control form-control-user" placeholder="Deskripsi Item">
-                        <?= form_error('deskripsi', '<small class="text-danger">', '<small>'); ?>
-                    </div>
-                    <center>
-                        <input class="btn btn-primary btn-user btn-block mt-4 w-50 center" type="submit" name="btnSubmit" value="Tambah Item" /> 
-                    </center>
-                </div>
-            </form>
-        </div>
-
+            </div>
+          </div>
     </div>
-</div>
+
+  </main>
