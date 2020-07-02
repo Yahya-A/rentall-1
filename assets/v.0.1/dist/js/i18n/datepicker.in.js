c2a2958b4dc3113673655db1b2b24d58145e2000
Yahya <61956371+Yahya-A@users.noreply.github.com>
2020-07-02 23:36:48 +0700

    datepicker

---
+++ assets/v.0.1/dist/js/i18n/datepicker.in.js
@@ -0,0 +1,15 @@
+;
+(function ($) {
+	$.fn.datepicker.language['in'] = {
+		days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+		daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+		daysMin: ['M', 'S', 'S', 'R', 'K', 'J', 'S'],
+		months: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+		monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des'],
+		today: 'Hari Ini',
+		clear: 'Clear',
+		dateFormat: 'yyyy/mm/dd',
+		timeFormat: 'hh:ii aa',
+		firstDay: 0
+	};
+})(jQuery);
